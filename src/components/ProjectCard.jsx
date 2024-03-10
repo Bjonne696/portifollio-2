5d9759362d28cc0ad48a40a493a7f3b802bf179e
Bjonne <Bjonne696@gmail.com>
2024-03-10 18:40:20 +0100

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const ProjectCard = ({ project }) => {
   return (
     <div className="block sm:w-96 md:w-72 lg:w-96 xl:w-96 2xl:w-1/4 border rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 flex flex-col justify-between" style={{ backgroundColor: '#fff750' }}>
-      {/* Title at the top of the card */}
-      <div className="p-6 bg-white">
+      <div className="p-6">
         <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
       </div>
       <div className="flex-grow">
@@ -13,7 +12,7 @@ const ProjectCard = ({ project }) => {
           <p className="text-gray-600 text-md mt-2">{project.info}</p>
         </div>
       </div>
-      <div className="p-6 bg-white bg-opacity-90">
+      <div className="p-6">
         <a href={project.repoUrl} className="text-blue-500 hover:underline">GitHub Repo</a>
         {project.liveUrl && (
           <a href={project.liveUrl} className="ml-4 text-blue-500 hover:underline">Live Site</a>
