c5728c822984674069efd2063de30e03c986546d
Bjonne <Bjonne696@gmail.com>
2024-03-10 18:24:24 +0100

    Update projectsData.jsx

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -4,7 +4,7 @@ const Projects = [
     {
       id: 1,
       title: 'Semester Project 2',
-      info: 'This project involves creating an advanced web application using modern development tools and frameworks.',
+      info: 'Auction Zone is a web-based auction platform aimed at Noroff Academy students, offering secure listing and bidding with a focus on community engagement and credit-based transactions, though it currently lacks several intended features such as real-time updates and comprehensive search filters.',
       imageUrl: 'https://github.com/Bjonne696/portifollio-2/blob/main/src/assets/semester2.png?raw=true',
       repoUrl: 'https://github.com/Bjonne696/Semester-Project-2.git',
       liveUrl: 'https://gentle-hummingbird-705347.netlify.app'
@@ -12,7 +12,7 @@ const Projects = [
     {
       id: 2,
       title: 'JavaScript Frameworks CA',
-      info: 'A comprehensive project showcasing the use of various JavaScript frameworks to solve complex problems.',
+      info: 'ShopEase is an intuitive e-commerce platform built with React, offering users a seamless shopping experience with features like a comprehensive product catalog, advanced search capabilities, and an effortless checkout process, all designed to simplify online shopping without sacrificing functionality',
       imageUrl: 'https://github.com/Bjonne696/portifollio-2/blob/main/src/assets/frameworks.png?raw=true',
       repoUrl: 'https://github.com/Bjonne696/course-assignment-front-end-frameworks.git',
       liveUrl: 'https://willowy-madeleine-6434a9.netlify.app'
@@ -22,7 +22,7 @@ const Projects = [
       title: 'Project Exam 2',
       info: 'The culmination of learnings in a practical exam project, implementing best practices and innovative solutions.',
       imageUrl: 'https://github.com/Bjonne696/portifollio-2/blob/main/src/assets/holidaze.png?raw=true',
-      repoUrl: 'https://github.com/Bjonne696/project-exam-2-holidaze.git',
+      repoUrl: 'Holidaze is a React-based accommodation booking site offering a dual interface for customers to book accommodations and for venue managers to manage listings, integrating seamlessly with the Noroff API for an efficient and user-friendly experience.',
       liveUrl: ' https://mellifluous-genie-846ce1.netlify.app'
     },
   ];
