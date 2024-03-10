96637fc1aa1b212479414dc07b3f0eb12b6c5aff
Bjonne <Bjonne696@gmail.com>
2024-03-10 19:19:03 +0100

    Actually finished

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ import projects from '../data/projectsData';
 const Portfolio = () => {
   return (
     <div className="min-h-screen p-10">
-      <h1 className="text-white text-4xl font-bold mb-8">This is Bjørn-Tore's Portfolio</h1>
+      <p className="text-white text-4xl font-bold mb-8">Click the links to be navigated to the repositories, or check out the live site!</p>
       <div className="flex flex-wrap justify-center gap-4">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
