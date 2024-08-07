61538ef47ac455743a73be85178ea9a5681c89a1
Bjonne <Bjonne696@gmail.com>
2024-08-07 21:58:08 +0200

    Digonal fixed
    Fixed creating diagonal background design in Homepage.jsx
    this was fixed by creating a custom class in tailwind called custom-skew in index.css, using transform and transform origin

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,11 +4,11 @@ import projects from '../data/projectsData';
 
 const Portfolio = () => {
   return (
-    <div className="min-h-screen flex relative overflow-hidden">
+    <div className="min-h-screen flex relative overflow-hidden bg-blue-500">
       {/* Left Side: Profile Image and Info with Diagonal Background */}
-      <div className="relative w-1/5 p-6 text-white flex-shrink-0">
-        <div className="absolute top-0 left-0 w-full h-full transform -skew-x-12 origin-bottom-right bg-blue-500"></div>
-        <div className="relative z-10 p-6">
+      <div className="relative w-1/4 flex-shrink-0">
+        <div className="absolute top-0 left-0 w-full h-full transform custom-skew origin-bottom-right bg-blue-500"></div>
+        <div className="relative z-10 p-6 text-white">
           <img
             src="https://via.placeholder.com/150"
             alt="Profile"
@@ -23,7 +23,7 @@ const Portfolio = () => {
       </div>
 
       {/* Right Side: Project Cards */}
-      <div className="w-1/2 bg-green-500 flex flex-col items-start space-y-4 p-6">
+      <div className="flex-grow bg-green-500 flex flex-col items-start space-y-4 p-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
