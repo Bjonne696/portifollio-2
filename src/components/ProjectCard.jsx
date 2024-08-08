85cbf8b42f4d7e3a6a68c9e3c9dfd08e3017675f
Bjonne <Bjonne696@gmail.com>
2024-08-08 20:35:01 +0200

    Completed design with diagonal stacked cards
    Created custom rule in index.css using transform: skewX

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,8 @@
 const ProjectCard = ({ project }) => {
   return (
-    <div className="block w-full border rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:bg-yellow-300 flex flex-col justify-between">
+    <div className="block w-full overflow-hidden shadow-lg transform transition duration-500 hover:bg-yellow-300 flex flex-col justify-between">
       <div className="p-6">
-        <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+        <h3 className="text-xl font-semibold reverse-custom-skew mb-2">{project.title}</h3>
       </div>
     </div>
   );
